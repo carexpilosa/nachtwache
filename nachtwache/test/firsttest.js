d1f0225c73a8f19001521083331bbfa5fc095d56
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-05-04 16:31:07 +0200

    init

---
+++ nachtwache/test/firsttest.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'Demo test firsttest' : function (browser) {
+    browser
+      .url('http://localhost:8383/nachtwache/index.html')
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('input[name=testInput]', 1000)
+      .clearValue('input[name=testInput]')
+      .setValue('input[name=testInput]', 'nightwatch')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.value('input[name=testInput]', 'nightwatch')
+      .assert.containsText('h3#a', 'Sorbische Ostereier')
+      .end();
+  }
+};
